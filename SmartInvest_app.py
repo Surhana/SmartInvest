5b1844aa2d53318485415b9a147b3a3ae060bd6f
Surhana <surhanaamani8@gmail.com>
2025-08-30 02:47:46 +0800

    Update SmartInvest_app.py

--- SmartInvest_app.py
+++ SmartInvest_app.py
@@ -35,7 +35,7 @@ else:
 # Ensure first column is Alternative
 df = df.rename(columns={df.columns[0]: "Alternative"})
 
-st.subheader("The Data")
+st.subheader(" Data")
 st.dataframe(df)
 
 alternatives = df["Alternative"]
